3870e1a31b2650e30375598628b1e785bde8fd7f
wujianP <1051233700@qq.com>
2023-07-21 15:06:05 +0800

    test

--- generate_caption/dataset.py
+++ generate_caption/dataset.py
@@ -1,8 +1,10 @@
 import torch
+import json
 from torch.utils.data import Dataset
 
 
 class CaptionDataset(Dataset):
+    """dataset for Kinetics400 frame-level captions"""
     def __init__(self, caption_path, prompt_template):
         self.caption_path = caption_path
         self.prompt_template = prompt_template
@@ -17,4 +19,28 @@ class CaptionDataset(Dataset):
         caption = str(self.cap_dict[filename]).replace('\'', '')
         action = filename.split('/')[-2].replace('_', ' ')
         prompt = self.prompt_template + caption + f', {action}, \nOutput:'
+
         return filename, prompt, caption, action
+
+
+class SMiTCaptionDataset(Dataset):
+    """dataset for S-MiT original captions"""
+    def __init__(self, caption_path, prompt_template):
+        assert caption_path.endswith('.json')
+        self.caption_path = caption_path
+        self.prompt_template = prompt_template
+        with open(caption_path, 'r') as file:
+            self.data_list = json.load(file)
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        data = self.data_list[index]
+        file_path = data['file_path']
+        caption = data['caption']
+        class_name = data['class_name']
+        file_name = data['file_name']
+        prompt = self.prompt_template + caption + ', Output:'
+
+        return file_path, prompt, caption, class_name, file_name
--- generate_caption/generate_hard_negative_caption.py
+++ generate_caption/generate_hard_negative_caption.py
@@ -5,10 +5,9 @@ This is a script that used to generate hard negative captions
 import argparse
 import torch
 import time
-import csv
 
 from fastchat.model import load_model
-from dataset import CaptionDataset
+from dataset import SMiTCaptionDataset
 from torch.utils.data import DataLoader
 
 
@@ -22,11 +21,11 @@ def main(args):
         args.max_gpu_memory,
         args.load_8bit,
         args.cpu_offloading,
-        revision=args.revision,)
+        revision=args.revision, )
 
     # load data
-    caption_dataset = CaptionDataset(caption_path=args.data_path,
-                                     prompt_template=args.prompt_template)
+    caption_dataset = SMiTCaptionDataset(caption_path=args.data_path,
+                                         prompt_template=args.prompt_template)
     caption_dataloader = DataLoader(dataset=caption_dataset,
                                     batch_size=args.batch_size,
                                     num_workers=8,
@@ -35,7 +34,7 @@ def main(args):
 
     # do inference
     total_iters = len(caption_dataloader)
-    for cur_iter, (filenames, prompts, captions, actions) in enumerate(caption_dataloader):
+    for cur_iter, (file_paths, prompts, captions, class_names, file_names) in enumerate(caption_dataloader):
         start_time = time.time()
         # tokenize
         inputs = tokenizer(prompts, padding=True, truncation=True, return_tensors="pt")
@@ -52,7 +51,8 @@ def main(args):
         )
 
         # decode
-        outputs = tokenizer.batch_decode(output_sequences, skip_special_tokens=True, spaces_between_special_tokens=False)
+        outputs = tokenizer.batch_decode(output_sequences, skip_special_tokens=True,
+                                         spaces_between_special_tokens=False)
 
         # post-process
         if model.config.is_encoder_decoder:
@@ -64,16 +64,16 @@ def main(args):
         batch_time = end_time - start_time
 
         # save file
-        with open(args.save_path, "a", newline="") as f:
-            writer = csv.writer(f)
-            for i in range(args.batch_size):
-                filename = filenames[i]
-                action = actions[i]
-                output = outputs[i].rstrip("\n")
-                writer.writerow((filename, action, output))
+        # with open(args.save_path, "a", newline="") as f:
+        #     writer = csv.writer(f)
+        #     for i in range(args.batch_size):
+        #         file_path = file_paths[i]
+        #         class_name = class_names[i]
+        #         output = outputs[i].rstrip("\n")
+        #         writer.writerow((file_path, class_name, output))
 
         print('Input:' + captions[0])
-        print('Action:' + actions[0])
+        print('Class:' + class_names[0])
         print('Output:' + outputs[0])
         print(f"Iteration:[ {cur_iter + 1}/{total_iters} ], Batch time:{batch_time:.3f}")
 
@@ -82,13 +82,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Data and Model
     parser.add_argument("--model-path", type=str, default="lmsys/vicuna-7b-v1.3")
-    parser.add_argument("--data-path", type=str, default="/DDN_ROOT/ytcheng/code/Open-VCLIP-V2/video_description_gen/back/caption_record.pth")
+    parser.add_argument("--data-path", type=str,
+                        default="/DDN_ROOT/ytcheng/code/Open-VCLIP-V2/video_description_gen/back/caption_record.pth")
     parser.add_argument("--save-path", type=str, required=True)
     # Hyper-parameters
     parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--prompt-template', type=str, required=True)
     # Devices
-    parser.add_argument("--device", type=str, choices=["cpu", "cuda", "mps", "xpu"], default="cuda", help="The device type")
+    parser.add_argument("--device", type=str, choices=["cpu", "cuda", "mps", "xpu"], default="cuda",
+                        help="The device type")
     parser.add_argument("--gpus", type=str, default=None, help="A single GPU like 1 or multiple GPUs like 0,2")
     parser.add_argument("--num-gpus", type=int, default=1)
     # LLM
@@ -96,14 +98,19 @@ if __name__ == '__main__':
     parser.add_argument("--repetition_penalty", type=float, default=1.0)
     parser.add_argument("--max-new-tokens", type=int, default=512)
     # Others
-    parser.add_argument("--revision", type=str, default="main",help="Hugging Face Hub model revision identifier")
+    parser.add_argument("--revision", type=str, default="main", help="Hugging Face Hub model revision identifier")
     parser.add_argument("--max-gpu-memory", type=str, help="The maximum memory per gpu. Use a string like '13Gib'")
     parser.add_argument("--load-8bit", action="store_true", help="Use 8-bit quantization")
-    parser.add_argument("--cpu-offloading", action="store_true", help="Only when using 8-bit quantization: Offload excess weights to the CPU that don't fit on the GPU")
-    parser.add_argument("--gptq-ckpt", type=str, default=None, help="Load quantized model. The path to the local GPTQ checkpoint.")
-    parser.add_argument("--gptq-wbits", type=int, default=16, choices=[2, 3, 4, 8, 16], help="#bits to use for quantization")
-    parser.add_argument("--gptq-groupsize", type=int, default=-1, help="Groupsize to use for quantization; default uses full row.")
-    parser.add_argument("--gptq-act-order", action="store_true", help="Whether to apply the activation order GPTQ heuristic")
+    parser.add_argument("--cpu-offloading", action="store_true",
+                        help="Only when using 8-bit quantization: Offload excess weights to the CPU that don't fit on the GPU")
+    parser.add_argument("--gptq-ckpt", type=str, default=None,
+                        help="Load quantized model. The path to the local GPTQ checkpoint.")
+    parser.add_argument("--gptq-wbits", type=int, default=16, choices=[2, 3, 4, 8, 16],
+                        help="#bits to use for quantization")
+    parser.add_argument("--gptq-groupsize", type=int, default=-1,
+                        help="Groupsize to use for quantization; default uses full row.")
+    parser.add_argument("--gptq-act-order", action="store_true",
+                        help="Whether to apply the activation order GPTQ heuristic")
 
     args = parser.parse_args()
 
