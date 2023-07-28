1c8df024cce4146ae8d68f59b2660a4eeb975e46
wujianP <1051233700@qq.com>
2023-07-28 16:26:23 +0800

    test

--- segment_video/dataset/rawframe_dataset.py
+++ segment_video/dataset/rawframe_dataset.py
@@ -8,14 +8,6 @@ class RawFrameDataset(Dataset):
         with open(path_file, 'r') as file:
             self.path_list = file.readlines()
 
-        if transform is None:
-            # self.transform = transforms.Compose([
-            #     transforms.Resize((256, 324))
-            # ])
-            pass
-        else:
-            self.transform = transform
-
     def __getitem__(self, idx):
         path = self.path_list[idx].strip()
         with open(path, 'rb') as f:
