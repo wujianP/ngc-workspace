3cbbf1745c6f98dc3d6b3694ba2847f6b7a603be
wujianP <1051233700@qq.com>
2023-07-31 14:12:20 +0800

    test

--- tools/extract_rawframes/generate_file_list.py
+++ tools/extract_rawframes/generate_file_list.py
@@ -3,7 +3,7 @@ import os
 
 def collect_image_paths(folder_path):
     image_paths = []
-    for filename in os.listdir(folder_path):
+    for filename in sorted(os.listdir(folder_path)):
         if filename.startswith("img_") and filename.endswith(".jpg"):
             image_path = os.path.join(folder_path, filename)
             image_paths.append(image_path)
@@ -19,7 +19,7 @@ def process_path_file(path_file, output_file):
     with open(output_file, 'w') as output:
         for folder_path in folder_paths:
             folder_path = folder_path[14:-5]  # discard post-fix '.jpg' and pre-fix '/dev/shm/k400/'
-            path_root = '/discobox/wjpeng/dataset/k400/rawframes/'
+            path_root = '/discobox/wjpeng/dataset/k400/raw_frames/'
             folder_path = os.path.join(path_root, folder_path)
             if os.path.isdir(folder_path):
                 image_paths = collect_image_paths(folder_path)
@@ -31,6 +31,6 @@ def process_path_file(path_file, output_file):
 
 # Example usage:
 if __name__ == '__main__':
-    path_file_path = '/discobox/wjpeng/code/202306/ngc-workspace/tools/generate_image_list/video_path.txt'  # Replace with the actual path to your path.txt file
-    output_file_path = '/discobox/wjpeng/code/202306/ngc-workspace/tools/generate_image_list/image_path.txt'  # Replace with the desired output file path
+    path_file_path = '/discobox/wjpeng/code/202306/ngc-workspace/tools/extract_rawframes/build_report_bk.txt'  # Replace with the actual path to your path.txt file
+    output_file_path = '/discobox/wjpeng/dataset/k400/ann/rawframe_list.txt'  # Replace with the desired output file path
     process_path_file(path_file_path, output_file_path)
