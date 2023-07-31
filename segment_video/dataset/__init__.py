191623f346e65b90a66013c2d9d03b7c19fbff5c
wujianP <1051233700@qq.com>
2023-07-31 14:00:28 +0800

    test

--- segment_video/dataset/__init__.py
+++ segment_video/dataset/__init__.py
@@ -1,4 +1,4 @@
-from .rawframe_dataset import RawFrameDataset
+from .rawframe_dataset import RawFrameDatasetSegFormer, RawFrameDatasetGroundingSAM
 
 
-__all__ = ['RawFrameDataset']
+__all__ = ['RawFrameDatasetSegFormer', RawFrameDatasetGroundingSAM]
--- segment_video/scripts/groundedSAM.sh
+++ segment_video/scripts/groundedSAM.sh
@@ -7,7 +7,7 @@ python groundedeSAM.py \
   --grounded_checkpoint $WEIGHT_PATH/groundingDINO/groundingdino_swint_ogc.pth \
   --sam_hq_checkpoint $WEIGHT_PATH/hq-sam/sam_hq_vit_b.pth \
   --use_sam_hq \
-  --input_image /discobox/wjpeng/code/202306/ngc-workspace/segment_video/demo_images/12.png \
+  --image_path /discobox/wjpeng/code/202306/ngc-workspace/segment_video/demo_images/12.png \
   --output_dir /discobox/wjpeng/code/202306/ngc-workspace/segment_video/outputs \
   --box_threshold 0.3 \
   --text_threshold 0.25 \
