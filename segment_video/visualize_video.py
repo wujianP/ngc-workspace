0b3bc4717afa44693abd1509de7bbfa3f42e30c7
wujianP <1051233700@qq.com>
2023-08-08 13:00:35 +0800

    add frequently visualize logics

--- segment_video/visualize_video.py
+++ segment_video/visualize_video.py
@@ -25,16 +25,16 @@ if __name__ == '__main__':
     run = wandb.init('visualize-video')
     seg_ret_path = '/discobox/wjpeng/dataset/k400/ann/groundedSAM/DINO-SwinT-imgSize800_SAMHQ-ViTB-imgSize1024_stride8.npy'
     seg_ret = np.load(seg_ret_path, allow_pickle=True).item()
-    video_name = ['train/dancing_ballet/8jS_PCZUO3A_000062_000072',
-                  'train/shaking_hands/iQVa2URAuAU_000006_000016',
-                  'train/trimming_trees/ExQg288lBrI_000000_000010',
-                  'train/brushing_teeth/fmecM7Fx1tQ_000045_000055',
-                  'train/petting_cat/rF6WHdvfhzU_000044_000054',
-                  'train/parkour/FESQKVYD92Q_000003_000013',
-                  'train/train/yoga/UX3k9_185Ns_000069_000079',
-                  'train/washing_hands/Hwx_8FhcqY4_000074_000084']
-    for i in range(len(video_name)):
-        video_name = video_name[i]
+    video_names = ['train/dancing_ballet/8jS_PCZUO3A_000062_000072',
+                   'train/shaking_hands/iQVa2URAuAU_000006_000016',
+                   'train/trimming_trees/ExQg288lBrI_000000_000010',
+                   'train/brushing_teeth/fmecM7Fx1tQ_000045_000055',
+                   'train/petting_cat/rF6WHdvfhzU_000044_000054',
+                   'train/parkour/FESQKVYD92Q_000003_000013',
+                   'train/train/yoga/UX3k9_185Ns_000069_000079',
+                   'train/washing_hands/Hwx_8FhcqY4_000074_000084']
+    for i in range(len(video_names)):
+        video_name = video_names[i]
         masks, frames = load_masks_frames(seg_ret, video_name)
         mask_frames = []
         for mask, frame in zip(masks, frames):
