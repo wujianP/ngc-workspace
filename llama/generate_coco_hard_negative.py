d3d4d881840b19d653ce11a33778c7316ed19820
wujianP <1051233700@qq.com>
2023-08-10 16:42:41 +0800

    add llama

--- llama/generate_coco_hard_negative.py
+++ llama/generate_coco_hard_negative.py
@@ -31,7 +31,7 @@ def process_captions(captions, prompt):
 
 def main(args):
     for key, value in vars(args).items():
-        print(f'key: {value}')
+        print(f'{key}: {value}')
     os.makedirs(os.path.dirname(args.output_dir), exist_ok=True)
 
     generator = Llama.build(
