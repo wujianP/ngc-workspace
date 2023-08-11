e0004814642b7e03e7669da5d28f491eb88a4aea
wujianP <1051233700@qq.com>
2023-08-11 15:18:03 +0800

    add prompt to prompt

--- llama/script.sh
+++ llama/script.sh
@@ -7,4 +7,4 @@ torchrun --nproc_per_node 1 --master_port 29500 generate_coco_hard_negative.py \
 --output_dir /discobox/wjpeng/dataset/coco2014/hardNegatives/train \
 --save_freq 500 \
 --prompt "In this task, you need to modify the given sentence to change its meaning. You can alter a word or phrase (noun, adjective, verb, preposition, etc.), or swap the positions of two words. Ensure that the sentence's meaning is altered, and guarantee that the modified sentences are not significantly different from the original one." \
---filename coco2014train_hardNegative_llama-2-7b-chat.pth
+--filename coco2014train_hardNegative_llama-2-7b-chat
