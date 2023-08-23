fc19abe3354cf9ec76cd8f575d62ac9b9062c78c
wujianP <1051233700@qq.com>
2023-08-24 00:06:24 +0800

    add prompt to prompt

--- llama/tool.py
+++ llama/tool.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     coco = CocoDataset(image_root='/discobox/wjpeng/dataset/coco2014/images/train2014',
                        json='/discobox/wjpeng/dataset/coco2014/annotations/captions_train2014.json')
     output = '/discobox/wjpeng/dataset/coco2014/captions_demo.csv'
-    print(len(coco))
     for i in range(len(coco)):
-        if i % 10 == 0:
+        if i % 100 == 0:
             cap, _ = coco[i]
             with open(output, mode='a') as f:
                 f.write(cap + '\n')
             print(f'{100*i/len(coco):.2f}%')
+    print(len(coco))
