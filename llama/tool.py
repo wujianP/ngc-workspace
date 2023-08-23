ef25f21ee788279c96e259028c00da061a370ba6
wujianP <1051233700@qq.com>
2023-08-24 00:11:07 +0800

    add prompt to prompt

--- llama/tool.py
+++ llama/tool.py
@@ -4,7 +4,7 @@ from dataset import CocoDataset
 if __name__ == '__main__':
     coco = CocoDataset(image_root='/discobox/wjpeng/dataset/coco2014/images/train2014',
                        json='/discobox/wjpeng/dataset/coco2014/annotations/captions_train2014.json')
-    output = '/discobox/wjpeng/dataset/coco2014/captions_demo.csv'
+    output = '/discobox/wjpeng/dataset/coco2014/captions_demo.txt'
     for i in range(len(coco)):
         if i % 100 == 0:
             cap, _ = coco[i]
