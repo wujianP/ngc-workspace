1c5e53b1eb66316ddb6892f81dcf10237ce055cc
wujianP <1051233700@qq.com>
2023-09-07 13:26:02 +0800

    test

--- Grounded-Segment-Anything/wj_inpaint_ram.py
+++ Grounded-Segment-Anything/wj_inpaint_ram.py
@@ -1,61 +1,63 @@
 import argparse
 import os
-
 import json
 import torch
 import torchvision
-from PIL import Image
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms as TS
 
 # Grounding DINO
-import GroundingDINO.groundingdino.datasets.transforms as T
 from GroundingDINO.groundingdino.models import build_model
 from GroundingDINO.groundingdino.util.slconfig import SLConfig
 from GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
 
 # segment anything
-from segment_anything import (
-    build_sam,
-    build_sam_hq,
-    SamPredictor
-) 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+from segment_anything import build_sam, SamPredictor, build_sam_hq, build_sam_hq_vit_l
+from segment_anything.utils.transforms import ResizeLongestSide
 
-# Recognize Anything Model & Tag2Text
+# Tag2Text
 import sys
+
 sys.path.append('Tag2Text')
 from Tag2Text.models import tag2text
 from Tag2Text import inference_ram
-import torchvision.transforms as TS
+
+# BLIP-2
+from transformers import Blip2Processor, Blip2ForConditionalGeneration
+
+# wandb
+import wandb
+
+wandb.login()
+run = wandb.init('Tag2Text & Grounded DINO & HQ-SAM & Visual Genome')
+
+from datasets import load_dataset
+from diffusers import StableDiffusionInpaintPipeline
+from PIL import Image
 from torch.utils.data import DataLoader
 
-from dataset import CoCoDataset
 
-import wandb
-wandb.login()
-run = wandb.init('RAM & Grounded DINO & HQ-SAM')
+# def my_collate_fn(batch):
+#     images, Ws, Hs, paths = [], [], [], []
+#     for item in batch:
+#         images.append(item[0])
+#         Ws.append(item[1])
+#         Hs.append(item[2])
+#         paths.append(item[3])
+#     return [images, Ws, Hs, paths]
 
 
 def my_collate_fn(batch):
-    images, Ws, Hs, paths = [], [], [], []
+    images, image_ids, Ws, Hs, objects = [], [], [], [], []
     for item in batch:
-        images.append(item[0])
-        Ws.append(item[1])
-        Hs.append(item[2])
-        paths.append(item[3])
-    return [images, Ws, Hs, paths]
-
-
-def check_tags_chinese(tags_chinese, pred_phrases, max_tokens=100, model="gpt-3.5-turbo"):
-    object_list = [obj.split('(')[0] for obj in pred_phrases]
-    object_num = []
-    for obj in set(object_list):
-        object_num.append(f'{object_list.count(obj)} {obj}')
-    object_num = ', '.join(object_num)
-    print(f"Correct object number: {object_num}")
-
-    return tags_chinese
+        images.append(item['image'])
+        image_ids.append(item['image_id'])
+        Ws.append(item['width'])
+        Hs.append(item['height'])
+        objects.append(item['objects'])
+    return [images, image_ids, Ws, Hs, objects]
 
 
 def load_grounding_dino_model(model_config_path, model_checkpoint_path):
@@ -70,46 +72,43 @@ def load_grounding_dino_model(model_config_path, model_checkpoint_path):
     return model.cuda()
 
 
-def get_grounding_output(model, image, caption, box_threshold, text_threshold,device="cpu"):
-    caption = caption.lower()
-    caption = caption.strip()
-    if not caption.endswith("."):
-        caption = caption + "."
-    model = model.to(device)
-    image = image.to(device)
+def get_grounding_output(model, image, caption, box_threshold, text_threshold, with_logits=True):
+    # preprocess captions
+    for k in range(len(caption)):
+        caption[k] = caption[k].lower().strip()
+        if not caption[k].endswith("."):
+            caption[k] = caption[k] + "."
+    # forward grounded dino
     with torch.no_grad():
-        outputs = model(image[None], captions=[caption])
-    logits = outputs["pred_logits"].cpu().sigmoid()[0]  # (nq, 256)
-    boxes = outputs["pred_boxes"].cpu()[0]  # (nq, 4)
-    logits.shape[0]
-
-    # filter output
-    logits_filt = logits.clone()
-    boxes_filt = boxes.clone()
-    filt_mask = logits_filt.max(dim=1)[0] > box_threshold
-    logits_filt = logits_filt[filt_mask]  # num_filt, 256
-    boxes_filt = boxes_filt[filt_mask]  # num_filt, 4
-    logits_filt.shape[0]
-
-    # get phrase
-    tokenlizer = model.tokenizer
-    tokenized = tokenlizer(caption)
-    # build pred
-    pred_phrases = []
-    scores = []
-    for logit, box in zip(logits_filt, boxes_filt):
-        pred_phrase = get_phrases_from_posmap(logit > text_threshold, tokenized, tokenlizer)
-        pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
-        scores.append(logit.max().item())
-
-    return boxes_filt, torch.Tensor(scores), pred_phrases
+        outputs = model(image, captions=caption)
+    logits = outputs["pred_logits"].cpu().sigmoid()  # (bs, nq, 256)
+    boxes = outputs["pred_boxes"].cpu()  # (bs, nq, 4)
+    # post process
+    boxes_list, scores_list, phrases_list = [], [], []
+    for ub_logits, ub_boxex, cap in zip(logits, boxes, caption):
+        mask = ub_logits.max(dim=1)[0] > box_threshold
+        logits_filtered = ub_logits[mask]  # (n, 256)
+        boxes_filtered = ub_boxex[mask]  # (n, 4)
+        phrases_filtered = []
+        scores_filtered = []
+        for logit, box in zip(logits_filtered, boxes_filtered):
+            pred_phrase = get_phrases_from_posmap(logit > text_threshold, model.tokenizer(cap), model.tokenizer)
+            if with_logits:
+                phrases_filtered.append(pred_phrase + f"({str(logit.max().item())[:4]})")
+            else:
+                phrases_filtered.append(pred_phrase)
+            scores_filtered.append(logit.max().item())
+        boxes_list.append(boxes_filtered)
+        scores_list.append(torch.Tensor(scores_filtered))
+        phrases_list.append(phrases_filtered)
+    return boxes_list, scores_list, phrases_list
 
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
@@ -118,18 +117,67 @@ def show_mask(mask, ax, random_color=False):
 def show_box(box, ax, label):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2)) 
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
     ax.text(x0, y0, label)
-    
+
+
+def prepare_sam_data(images, boxes, Hs, Ws, resize_size):
+    resize_transform = ResizeLongestSide(resize_size)
+
+    def prepare_image(image, transform):
+        image = np.array(image)
+        image = transform.apply_image(image)
+        image = torch.as_tensor(image).cuda()
+        return image.permute(2, 0, 1).contiguous()
+
+    batched_input = []
+    for i in range(len(images)):
+        data = {
+            'image': prepare_image(images[i], resize_transform),
+            'original_size': (Hs[i], Ws[i])
+        }
+        if boxes[i].shape[0] > 0:
+            data['boxes'] = resize_transform.apply_boxes_torch(boxes[i], (Hs[i], Ws[i]))
+        batched_input.append(data)
+    return batched_input
+
+
+def wandb_visualize(images, tags, boxes_filt, masks_list, pred_phrases):
+    for i in range(len(images)):
+        img, tag, boxes, masks, labels = images[i], tags[i], boxes_filt[i], masks_list[i], \
+        pred_phrases[i]
+        if len(boxes) > 0:
+            fig, ax = plt.subplots(1, 3, figsize=(10, 10))
+            # show image only
+            ax[0].imshow(img)
+            ax[0].axis('off')
+            # show boxes, masks, image
+            ax[1].imshow(img)
+            for (box, label) in zip(boxes, labels):
+                show_box(box.cpu().numpy(), ax[1], label)
+            for mask in masks:
+                show_mask(mask, ax[1], random_color=True)
+            # mask_all = np.logical_or.reduce(masks, axis=0)
+            # show_mask(mask_all, ax[1], random_color=True)
+            ax[1].axis('off')
+            # show masks only
+            for mask in masks:
+                show_mask(mask, ax[2], random_color=True)
+            ax[2].axis('off')
+            # send to wandb
+            plt.tight_layout()
+            run.log({'Visualization': wandb.Image(plt.gcf(), caption=f'Tags: {tag}')})
+            plt.close()
+
 
 if __name__ == "__main__":
-    # args
+
     parser = argparse.ArgumentParser("Grounded-Segment-Anything Demo", add_help=True)
     parser.add_argument("--config", type=str, required=True, help="path to config file")
     parser.add_argument("--ram_checkpoint", type=str, required=True, help="path to checkpoint file")
     parser.add_argument("--grounded_checkpoint", type=str, required=True, help="path to checkpoint file")
-    parser.add_argument("--sam_checkpoint", type=str, required=True, help="path to checkpoint file")
-    parser.add_argument("--sam_hq_checkpoint", type=str, default=None, help="path to sam-hq checkpoint file")
+    parser.add_argument("--sam_checkpoint", type=str, help="path to checkpoint file")
+    parser.add_argument("--sam_hq_checkpoint", type=str, help="path to checkpoint file")
     parser.add_argument("--use_sam_hq", action="store_true", help="using sam-hq for prediction")
     parser.add_argument("--split", default=",", type=str, help="split for text prompt")
     parser.add_argument("--output_dir", "-o", type=str, default="outputs", required=True, help="output directory")
@@ -142,105 +190,126 @@ if __name__ == "__main__":
     parser.add_argument("--data_ann", type=str)
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--num_workers", type=int, default=8)
+
+    parser.add_argument("--user_specified_tags", type=str, default="None",
+                        help="user specified tags for tag2text, if more than one, use ',' to split")
+    parser.add_argument("--grounding_dino_img_size", type=int, default=800)
+
     args = parser.parse_args()
 
     # cfg
-    config_file = args.config  # change the path of the model config file
+    config_file = args.config
     device = "cuda"
 
     # load data
-    dataset = CoCoDataset(image_root=args.data_root, json=args.data_ann)
+    # dataset = CoCoDataset(image_root=args.data_root, json=args.data_ann)
+    dataset = load_dataset(path="visual_genome", name="objects_v1.2.0",
+                           split="train")
     dataloader = DataLoader(dataset=dataset,
                             batch_size=args.batch_size,
                             num_workers=args.num_workers,
                             pin_memory=True,
                             shuffle=False,
                             collate_fn=my_collate_fn)
-    from IPython import embed
-    embed()
-    # image_pil, image = load_image(image_path)
 
     # load Grounded-DINO model
     grounding_dino_model = load_grounding_dino_model(config_file, args.grounded_checkpoint)
 
-    # load RAM model
-    ram_model = tag2text.ram(pretrained=args.ram_checkpoint, image_size=384, vit='swin_l')
-    ram_model.eval()
-    ram_model = ram_model.to(device)
-
-    # initialize Recognize Anything Model
-    normalize = TS.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    transform = TS.Compose([TS.Resize((384, 384)), TS.ToTensor(), normalize])
-
-    # threshold for tagging
-    # we reduce the threshold to obtain more tags
-    raw_image = image_pil.resize((384, 384))
-    raw_image = transform(raw_image).unsqueeze(0).to(device)
-
-    res = inference_ram.inference(raw_image, ram_model)
-
-    # Currently ", " is better for detecting single tags
-    # while ". " is a little worse in some case
-    tags = res[0].replace(' |', ',')
-    tags_chinese = res[1].replace(' |', ',')
-
-    # print("Image Tags: ", res[0])
-    # print("图像标签: ", res[1])
-
-    # run grounding dino model
-    boxes_filt, scores, pred_phrases = get_grounding_output(
-        model, image, tags, args.box_threshold, args.text_threshold, device=device
-    )
-
-    # initialize SAM
+    # load hq-SAM
     if args.use_sam_hq:
-        print("Initialize SAM-HQ Predictor")
-        predictor = SamPredictor(build_sam_hq(checkpoint=args.sam_hq_checkpoint).to(device))
+        sam = build_sam_hq_vit_l(checkpoint=args.sam_hq_checkpoint).cuda()
     else:
-        predictor = SamPredictor(build_sam(checkpoint=args.sam_checkpoint).to(device))
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    predictor.set_image(image)
+        sam = build_sam(checkpoint=args.sam_checkpoint).cuda()
 
-    size = image_pil.size
-    H, W = size[1], size[0]
-    for i in range(boxes_filt.size(0)):
-        boxes_filt[i] = boxes_filt[i] * torch.Tensor([W, H, W, H])
-        boxes_filt[i][:2] -= boxes_filt[i][2:] / 2
-        boxes_filt[i][2:] += boxes_filt[i][:2]
+    # load RAM model
+    ram_model = tag2text.ram(pretrained=args.ram_checkpoint,
+                             image_size=384,
+                             vit='swin_l').cuda()
+    ram_model.eval()
 
-    boxes_filt = boxes_filt.cpu()
-    # use NMS to handle overlapped boxes
-    print(f"Before NMS: {boxes_filt.shape[0]} boxes")
-    nms_idx = torchvision.ops.nms(boxes_filt, scores, args.iou_threshold).numpy().tolist()
-    boxes_filt = boxes_filt[nms_idx]
-    pred_phrases = [pred_phrases[idx] for idx in nms_idx]
-    print(f"After NMS: {boxes_filt.shape[0]} boxes")
-    tags_chinese = check_tags_chinese(tags_chinese, pred_phrases)
-    print(f"Revise tags_chinese with number: {tags_chinese}")
+    # iterate forward pass
+    total_iter = len(dataloader)
+    result_dict = {'configure': vars(args)}
+    # for iter_idx, (images, Ws, Hs, paths) in enumerate(dataloader):
+    for iter_idx, (images, image_ids, Ws, Hs, objects) in enumerate(dataloader):
 
-    transformed_boxes = predictor.transform.apply_boxes_torch(boxes_filt, image.shape[:2]).to(device)
+        from IPython import embed
+        embed()
+        # >>> Tagging: inference RAM >>>
+        trans_ram = TS.Compose([TS.Resize((384, 384)),
+                                     TS.ToTensor(),
+                                     TS.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        ram_images = torch.stack([trans_ram(img) for img in images]).cuda()
+        ram_ret = inference_ram.inference(image=ram_images, model=ram_model)
 
-    masks, _, _ = predictor.predict_torch(
-        point_coords = None,
-        point_labels = None,
-        boxes = transformed_boxes.to(device),
-        multimask_output = False,
-    )
-    
-    # draw output image
-    plt.figure(figsize=(10, 10))
-    plt.imshow(image)
-    for mask in masks:
-        show_mask(mask.cpu().numpy(), plt.gca(), random_color=True)
-    for box, label in zip(boxes_filt, pred_phrases):
-        show_box(box.numpy(), plt.gca(), label)
+        tags_list = [tag.replace(' |', ',') for tag in ram_ret[0]]
+        tags_chinese = [tag.replace(' |', ',') for tag in ram_ret[1]]
+        # empty cache
+        torch.cuda.empty_cache()
 
-    # plt.title('RAM-tags' + tags + '\n' + 'RAM-tags_chineseing: ' + tags_chinese + '\n')
-    plt.axis('off')
-    plt.savefig(
-        os.path.join(args.output_dir, "automatic_label_output.jpg"),
-        bbox_inches="tight", dpi=300, pad_inches=0.0
-    )
+        # >>> Detection: inference grounded dino >>>
+        # preprocess images
+        trans_grounded = TS.Compose(
+            [
+                TS.Resize((args.grounding_dino_img_size, args.grounding_dino_img_size)),
+                TS.ToTensor(),
+                TS.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            ]
+        )
+        dino_images = torch.stack([trans_grounded(img) for img in images], dim=0).cuda()
+        # forward grounded dino
+        boxes_filt_list, scores_list, pred_phrases_list = get_grounding_output(
+            model=grounding_dino_model,
+            image=dino_images,
+            caption=tags_list,
+            box_threshold=args.box_threshold,
+            text_threshold=args.text_threshold
+        )
+        # post process bounding box
+        for i in range(len(boxes_filt_list)):
+            H, W = Hs[i], Ws[i]
+            boxes = boxes_filt_list[i]
+            for k in range(boxes.size(0)):
+                boxes[k] = boxes[k] * torch.Tensor([W, H, W, H])
+                boxes[k][:2] -= boxes[k][2:] / 2
+                boxes[k][2:] += boxes[k][:2]
+            boxes_filt_list[i] = boxes.cuda()
+        # use NMS to handle overlapped boxes
+        for i in range(args.batch_size):
+            boxes_filt_list[i] = boxes_filt_list[i].cpu()
+            nms_idx = torchvision.ops.nms(boxes_filt_list[i], scores_list[i], args.iou_threshold).numpy().tolist()
+            boxes_filt_list[i] = boxes_filt_list[i][nms_idx].cuda()
+            pred_phrases_list[i] = [pred_phrases_list[i][idx] for idx in nms_idx]
+            # caption = check_caption(tag2text_caption, pred_phrases)
+        # empty cache
+        torch.cuda.empty_cache()
 
-    save_mask_data(args.output_dir, tags_chinese, masks, boxes_filt, pred_phrases)
+        # >>> Segmentation: inference sam >>>
+        # preprocess images
+        batched_input = prepare_sam_data(images=images, boxes=boxes_filt_list,
+                                         Hs=Hs, Ws=Ws,
+                                         resize_size=sam.image_encoder.img_size)
+        # forward sam
+        batched_output = sam(batched_input, multimask_output=False)
+        masks_list = [output['masks'].cpu().numpy() for output in batched_output]
+        # empty cache
+        torch.cuda.empty_cache()
+
+        # >>> Caption: BLIP-2
+        # processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+        # model = Blip2ForConditionalGeneration.from_pretrained(
+        #     "Salesforce/blip2-opt-2.7b", torch_dtype=torch.float32
+        # ).cuda()
+        # prompt = "Question: how many cats are there? Answer:"
+        # inputs = processor(images=images,  return_tensors="pt").to(device, torch.float16)
+        # generated_ids = model.generate(**inputs)
+        # generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+
+        # >>> Wandb visualize >>>
+        wandb_visualize(images, tags_list, boxes_filt_list, masks_list, pred_phrases_list)
+
+        # >>> Inpainting: inference stable diffusion or lama >>>
+
+        from IPython import embed
+
+        embed()
