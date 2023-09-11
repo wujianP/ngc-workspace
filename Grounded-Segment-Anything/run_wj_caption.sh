a644b352c138736ce9e5fa6ebfd56babf8f7a26d
wujianP <1051233700@qq.com>
2023-09-11 21:42:12 +0800

    add blip caption

---
+++ Grounded-Segment-Anything/run_wj_caption.sh
@@ -0,0 +1,9 @@
+conda activate /discobox/wjpeng/env/clip
+cd /discobox/wjpeng/code/202306/ngc-workspace/Grounded-Segment-Anything
+
+export CUDA_VISIBLE_DEVICES=1
+python wj_image_caption.py \
+--data_root /DDN_ROOT/wjpeng/dataset/inpainted-visual-genome/remove-stable-diffusion \
+--data_ann wjpeng/ann/remove-stable-diffusion-output-eval.json \
+--model_checkpoint /discobox/wjpeng/weights/blip2 \
+--outputs "outputs"
