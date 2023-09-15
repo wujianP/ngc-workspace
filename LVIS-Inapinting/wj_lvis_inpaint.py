9975e7e4d871378e4d77c174c8f0dff7f166c126
wujianP <1051233700@qq.com>
2023-09-15 14:37:30 +0800

    llama 2

---
+++ LVIS-Inapinting/wj_lvis_inpaint.py
@@ -0,0 +1,34 @@
+import argparse
+import torch
+import wandb
+wandb.login()
+
+from datasets import load_dataset
+from diffusers import StableDiffusionInpaintPipeline
+from PIL import Image
+
+
+def main():
+    # load dataset
+    dataset = load_dataset("winvoker/lvis", cache_dir=args.data_root)
+
+    # load Stable-Diffusion-Inpaint
+    inpaint_pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-2-inpainting",
+        torch_dtype=torch.float32,
+        cache_dir=args.model_checkpoint
+    ).to("cuda")
+
+    from IPython import embed
+    embed()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_root', type=str)
+    parser.add_argument('--model_checkpoint', type=str)
+    args = parser.parse_args()
+
+    run = wandb.init('LVIS-Inpaint')
+    main()
+    wandb.finish()
--- generate_caption/run_coco_hard_negative.sh
+++ generate_caption/run_coco_hard_negative.sh
@@ -30,24 +30,23 @@ python negCLIP_coco_hard_negative.py \
 --model-path 	lmsys/fastchat-t5-3b-v1.0 \
 --images_path /DDN_ROOT/wjpeng/dataset/coco2014/train2014 \
 --annotations_path /discobox/wjpeng/code/202306/fineCLIP/Bow/temp_data/train_neg_clip.tsv \
---batch-size 32 \
+--batch-size 16 \
 --outputs /DDN_ROOT/wjpeng/dataset/coco2014/annotations/100k/negative_caption_fastchat \
 --save_freq 50
 
-# >> on negCLIP 100k captions Not FastChat
-conda activate /discobox/wjpeng/env/clip/
-cd /discobox/wjpeng/code/202306/ngc-workspace/generate_caption
-git pull
-
-JOB_ID=0
-export CUDA_VISIBLE_DEVICES=$((JOB_ID % 8))
-python negCLIP_coco_hard_negative.py \
---job_id $JOB_ID \
---job_num 8 \
---model-path 	THUDM/chatglm2-6b \
---images_path /DDN_ROOT/wjpeng/dataset/coco2014/train2014 \
---annotations_path /discobox/wjpeng/code/202306/fineCLIP/Bow/temp_data/train_neg_clip.tsv \
---batch-size 8 \
---outputs /DDN_ROOT/wjpeng/dataset/coco2014/annotations/100k/negative_caption_chatglm2-6b \
---save_freq 50
-
+## >> on negCLIP 100k captions Not FastChat
+#conda activate /discobox/wjpeng/env/clip/
+#cd /discobox/wjpeng/code/202306/ngc-workspace/generate_caption
+#git pull
+#
+#JOB_ID=0
+#export CUDA_VISIBLE_DEVICES=$((JOB_ID % 8))
+#python negCLIP_coco_hard_negative.py \
+#--job_id $JOB_ID \
+#--job_num 8 \
+#--model-path 	THUDM/chatglm2-6b \
+#--images_path /DDN_ROOT/wjpeng/dataset/coco2014/train2014 \
+#--annotations_path /discobox/wjpeng/code/202306/fineCLIP/Bow/temp_data/train_neg_clip.tsv \
+#--batch-size 8 \
+#--outputs /DDN_ROOT/wjpeng/dataset/coco2014/annotations/100k/negative_caption_chatglm2-6b \
+#--save_freq 50
