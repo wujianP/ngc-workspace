25d0c43e83808fbec62f878cff1d426d517217d5
wujianP <1051233700@qq.com>
2023-09-15 16:48:23 +0800

    lvis

--- LVIS-Inapinting/dataset.py
+++ LVIS-Inapinting/dataset.py
@@ -1,4 +1,4 @@
-import json
+import os
 
 from PIL import Image
 from torch.utils.data import Dataset
@@ -8,7 +8,28 @@ from lvis import LVIS
 class LVISDataset(Dataset):
     def __init__(self, data_root, ann):
         self.data_root = data_root
-        ann = LVIS(ann)
-        self.images = ann.imgs
-        from IPython import embed
-        embed()
+        self.lvis = LVIS(ann)
+        self.image_ids = self.lvis.get_img_ids()
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        # load image
+        img_id = self.image_ids[idx]
+        img_dict = self.lvis.load_imgs([img_id])[0]
+        img_filename = '/'.join(img_dict['coco_url'].split('/')[-2:])
+        img_path = os.path.join(self.data_root, img_filename)
+        img = Image.open(img_path).convert('RGB')
+        # load masks
+        ann_ids = self.lvis.get_ann_ids(img_ids=[img_id])
+        ann_dicts = self.lvis.load_anns(ann_ids)
+        boxes, masks, areas = [], [], []
+        for ann_dict in ann_dicts:
+            mask = self.lvis.ann_to_mask(ann_dict)
+            box = ann_dict['bbox']  # [x,y,w,h]
+            area = ann_dict['area']
+            boxes.append(box)
+            masks.append(mask)
+            areas.append(area)
+        return boxes, masks, areas
