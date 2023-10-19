6babe507fa4ca400a12aac6fcf0f833e82762fe2
wujianP <1051233700@qq.com>
2023-10-19 11:58:19 +0800

    test

--- XPaste/run_wj.sh
+++ XPaste/run_wj.sh
@@ -6,4 +6,4 @@ python text2im.py \
 --model diffusers \
 --samples 200 \
 --category_file /DDN_ROOT/wjpeng/dataset/LVIS/annotations/coco/instances_train2017.json \
---output_dir /DDN_ROOT/wjpeng/dataset/instance_pool/LVIS_gen_FG
+--output_dir /DDN_ROOT/wjpeng/dataset/instance_pool/coco_stable_diff_GEN
